b876392a8066f288d54499fa1eb75a7ccb61b854
shafeequca <shafeequ.ca@gmail.com>
2017-06-12 23:53:10 +0530

    Test

--- InMag V.16/Program.cs
+++ InMag V.16/Program.cs
@@ -17,6 +17,7 @@ namespace InMag_V._16
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMenu());
+            //test
         }
     }
 }
